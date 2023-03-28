1cccd69c4a3aaf6ed2805bd5a1075025520d0cc0
trustytrojan <tarbot24@hotmail.com>
2023-03-28 13:21:50 -0400

    DataManager redone

--- src/main/java/discord/managers/DataManager.java
+++ src/main/java/discord/managers/DataManager.java
@@ -1,13 +1,13 @@
 package discord.managers;
 
 import java.util.Iterator;
-import java.util.concurrent.CompletableFuture;
 
-import discord.util.BetterJSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+
 import discord.util.BetterMap;
 import discord.client.DiscordClient;
 import discord.structures.DiscordObject;
-import discord.util.JSON;
 
 public abstract class DataManager<T extends DiscordObject> implements Iterable<T> {
 
@@ -22,22 +22,16 @@ public abstract class DataManager<T extends DiscordObject> implements Iterable<T
 		return cache.iterator();
 	}
 
-	/**
-	 * Creates a new object for this data, caches it,
-	 * and returns it. This is called {@code forceCache}
-	 * because it skips the cache check.
-	 * @param data data from the Discord API
-	 * @return the object
-	 */
-	protected abstract T forceCache(BetterJSONObject data);
+	protected abstract T cacheNewObject(JSONObject data);
 
 	/**
 	 * If an object has already been constructed and needs
 	 * to be cached, call this method.
+	 * 
 	 * @param t The object to cache
 	 * @return The same object
 	 */
-	public T cache(T t) {
+	public T cacheObject(T t) {
 		cache.put(t.id(), t);
 		return t;
 	}
@@ -46,47 +40,58 @@ public abstract class DataManager<T extends DiscordObject> implements Iterable<T
 	 * If the object that this data references is cached,
 	 * give this data to that object. If not, {@code forceCache}
 	 * is called.
+	 * 
 	 * @param data data from the Discord API
 	 * @return the object
 	 */
-	public T cache(BetterJSONObject data) {
+	public T cacheData(JSONObject data) {
 		final var fromCache = cache.get(data.getString("id"));
-		if (fromCache == null) return forceCache(data);
+		if (fromCache == null)
+			return cacheNewObject(data);
 		fromCache.setData(data);
 		return fromCache;
 	}
 
 	/**
-	 * The fetch methods available to the user that hide the path requirement.
+	 * This method calls {@code fetch(id, false)}.
+	 * 
+	 * @param id The ID of the resource to fetch from Discord.
+	 * @return The resource object of the subclass's type. The cache method
+	 *         is called to create the object and cache it.
 	 */
-	public CompletableFuture<T> fetch(String id) {
+	public T fetch(String id) {
 		return fetch(id, false);
 	}
 
 	/**
-	 * The fetch methods available to the user that hide the path requirement.
+	 * This method should call {@code fetch(String id, String path, boolean force)}.
+	 * 
+	 * @param id    The ID of the resource to fetch from Discord.
+	 * @param force Whether to skip the cache check
+	 * @return The resource object of the subclass's type. The cache method
+	 *         is called to create the object and cache it.
 	 */
-	public abstract CompletableFuture<T> fetch(String id, boolean force);
+	public abstract T fetch(String id, boolean force);
 
 	/**
 	 * Fetch a resource from Discord. This method should be called by the
 	 * subclass when implementing the above two versions of fetch().
 	 * 
-	 * @param id The ID of the resource to fetch from Discord.
-	 * @param path The API path to request. This should be provided by the subclass.
+	 * @param id    The ID of the resource to fetch from Discord.
+	 * @param path  The API path to request. This should be provided by the
+	 *              subclass.
 	 * @param force Whether to skip the cache check.
 	 * @return The resource object of the subclass's type. The cache method
-	 * is called to create the object and cache it.
+	 *         is called to create the object and cache it.
 	 */
-	protected CompletableFuture<T> fetch(String id, String path, boolean force) {
-		return CompletableFuture.supplyAsync(() -> {
-			if (!force) {
-				final var cached = cache.get(id);
-				if (cached != null) return cached;
-			}
-			final var data = JSON.parseObject(client.api.get(path));
-			return cache(data);
-		});
+	protected T fetch(String id, String path, boolean force) {
+		if (!force) {
+			final var cached = cache.get(id);
+			if (cached != null)
+				return cached;
+		}
+		final var data = JSON.parseObject(client.api.get(path));
+		return cacheData(data);
 	}
 
 }
