df71d0a119ea32b9c50bd40e0911f7d55f70674b
trustytrojan <tarbot24@hotmail.com>
2023-07-21 00:25:15 -0400

    Sj update

--- src/test/java/ActivityTracker.java
+++ src/test/java/ActivityTracker.java
@@ -1,7 +1,20 @@
+import java.util.Map;
+
 import discord.client.BotDiscordClient;
 import discord.resources.interactions.ChatInputInteraction;
+import sj.SjSerializable;
 
 public class ActivityTracker extends BotDiscordClient {
+	private static class ATGuild implements SjSerializable {
+		public final String guildId;
+		public final Map<String, Integer> activityPerMember;
+
+		@Override
+		public String toJsonString() {
+			
+		}
+	}
+
 	private ActivityTracker() {
 		ready.connect(() -> {
 			System.out.println("Logged in as " + user.tag() + '!');
