deac1ed39f68bdfffed0a06dc583bc9dc0c88de8
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2024-07-17 11:25:39 -0400

    `APIClient` restructuring; implemented (not tested) `multipart/form-data` POST requests

--- src/main/java/discord/client/APIClient.java
+++ src/main/java/discord/client/APIClient.java
@@ -1,9 +1,10 @@
 package discord.client;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandler;
@@ -24,27 +25,25 @@ import sj.SjObject;
  */
 public final class APIClient {
 	private static class DiscordAPIException extends RuntimeException {
-		DiscordAPIException(HttpRequestWithBody requestWrapper, HttpResponse<String> response) {
-			super(requestWrapper.request.method() + ' ' + requestWrapper.path + " -> " + response.statusCode()
-			    + "\nResponse body: " + response.body()
-				+ "\nRequest body: " + requestWrapper.body);
+		DiscordAPIException(Request req, HttpResponse<String> res) {
+			super(req.request.method() + ' ' + req.path + " -> " + res.statusCode()
+			    + "\nResponse body: " + res.body()
+				+ ((req.body != null) ? ("\nRequest body: " + req.body) : ""));
 		}
 	}
 
-	private static record HttpRequestWithBody(HttpRequest request, String path, String body) {}
-	private static enum HttpMethod { GET, POST, PUT, PATCH, DELETE };
-
+	private static record Request(HttpRequest request, String path, String body) {}
+	private static final String MULTIPART_FORMDATA_BOUNDARY = "discordjs4j";
 	private static final BodyHandler<String> BODY_HANDLER = BodyHandlers.ofString();
-
 	private static final HttpClient HTTP_CLIENT;
+	private static final String BASE_URL = "https://discord.com/api/v10";
+
 	static {
 		final var hcb = HttpClient.newBuilder();
 		if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) >= 19)
 			hcb.executor(Executors.newVirtualThreadPerTaskExecutor());
 		HTTP_CLIENT = hcb.build();
 	}
-
-	private static final String BASE_URL = "https://discord.com/api/v10";
 
 	public static class JsonResponse {
 		public final String rawText;
@@ -62,96 +61,126 @@ public final class APIClient {
 		}
 	}
 
-	public static record MultipartFormdataPart(String name, byte[] body, String filename) {}
+	public static class MultipartFormdataPart {
+		public final String name;
+		public String filename, contentType;
+		public final byte[] body;
+
+		public MultipartFormdataPart(final String name, final byte[] body) {
+			this.name = Objects.requireNonNull(name);
+			this.body = Objects.requireNonNull(body);
+		}
+
+		public byte[] toByteArray() throws IOException {
+			final var baos = new ByteArrayOutputStream();
+			baos.write("Content-Disposition: form-data; name=\"%s\"%s%s\n\n".formatted(
+				name,
+				(filename != null) ? ("; filename=\"" + filename + '"') : "",
+				(contentType != null) ? ("\nContent-Type: " + contentType) : ""
+			).getBytes());
+			baos.write(body);
+			return baos.toByteArray();
+		}
+	}
 
 	private final String token;
 	private final boolean debug;
 
 	APIClient(String token, boolean bot, boolean debug) {
-		Objects.requireNonNull(token);
 		if (token.isEmpty() || token.isBlank())
 			throw new IllegalArgumentException("Token cannot be empty or blank");
 		this.debug = debug;
 		this.token = bot ? ("Bot " + token) : token;
 	}
 
-	private HttpRequestWithBody buildRequest(HttpMethod method, String path, String body, String contentType) {
-		final var requestBuilder = HttpRequest.newBuilder(URI.create(BASE_URL + path));
-
-		BodyPublisher bp = null;
-		if (body != null) {
-			requestBuilder.header("Content-Type", contentType);
-			bp = BodyPublishers.ofString(body);
-		}
-
-		switch (method) {
-			case GET -> requestBuilder.GET();
-			case POST -> requestBuilder.POST(bp);
-			case PATCH -> requestBuilder.method("PATCH", bp);
-			case PUT -> requestBuilder.PUT(bp);
-			case DELETE -> requestBuilder.DELETE();
-		}
-
-		requestBuilder.header("Authorization", token);
-		return new HttpRequestWithBody(requestBuilder.build(), path, body);
+	private HttpRequest.Builder beginRequest(String path) {
+		return HttpRequest.newBuilder(URI.create(BASE_URL + path))
+			.header("Authorization", token);
 	}
 
-	private HttpRequestWithBody buildRequest(HttpMethod method, String path, String body) {
-		return buildRequest(method, path, body, "application/json");
-	}
-
-	private CompletableFuture<JsonResponse> sendRequest(HttpRequestWithBody requestWrapper) {
-		return HTTP_CLIENT.sendAsync(requestWrapper.request, BODY_HANDLER)
-			.thenApply(response -> {
-				final var statusCode = response.statusCode();
-				final var responseBody = response.body();
+	private CompletableFuture<JsonResponse> sendRequest(Request req) {
+		return HTTP_CLIENT.sendAsync(req.request, BODY_HANDLER)
+			.thenApply(res -> {
 				if (debug)
-					Logger.log(requestWrapper.request.method() + ' ' + requestWrapper.path + " -> " + statusCode);
-				if (statusCode == 429)
-					return retryAfter(requestWrapper, responseBody).join();
-				else if (statusCode >= 400)
-					throw new DiscordAPIException(requestWrapper, response);
-				return new JsonResponse(response.body());
+					Logger.log(req.request.method() + ' ' + req.path + " -> " + res.statusCode());
+
+				if (res.statusCode() == 429)
+					return retryAfter(req, res.body()).join();
+				else if (res.statusCode() >= 400)
+					throw new DiscordAPIException(req, res);
+
+				return new JsonResponse(res.body());
 			}).exceptionally(Util::printStackTrace);
 	}
 
-	private CompletableFuture<JsonResponse> retryAfter(HttpRequestWithBody requestWrapper, String responseBody) {
-		final var retryAfter = (int) (1000 * Sj.parseObject(responseBody).getDouble("retry_after"));
+	private CompletableFuture<JsonResponse> sendRequest(HttpRequest request, String path, String body) {
+		return sendRequest(new Request(request, path, body));
+	}
+
+	private CompletableFuture<JsonResponse> retryAfter(Request req, String resBody) {
+		final var retryAfterSec = Sj.parseObject(resBody).getInteger("retry_after");
 		if (debug)
-			Logger.log("Being rate limited for " + retryAfter + "ms");
-		return CompletableFuture.completedFuture(requestWrapper)
+			Logger.log("Being rate limited for " + retryAfterSec + " seconds");
+		return CompletableFuture.completedFuture(req)
 			.thenComposeAsync(
 				this::sendRequest,
-		    	CompletableFuture.delayedExecutor(retryAfter, TimeUnit.MILLISECONDS)
+				CompletableFuture.delayedExecutor(retryAfterSec, TimeUnit.SECONDS)
 			);
 	}
 
-	private CompletableFuture<JsonResponse> buildAndSend(HttpMethod method, String path, String body) {
-		return sendRequest(buildRequest(method, path, body));
-	}
-
 	public CompletableFuture<JsonResponse> get(String path) throws DiscordAPIException {
-		return buildAndSend(HttpMethod.GET, path, null);
+		return sendRequest(beginRequest(path).build(), path, null);
 	}
 
 	public CompletableFuture<JsonResponse> post(String path, String body) throws DiscordAPIException {
-		return buildAndSend(HttpMethod.POST, path, body);
+		return sendRequest(
+			beginRequest(path)
+				.POST(BodyPublishers.ofString(body))
+				.header("Content-Type", "application/json")
+				.build(),
+			path, body
+		);
 	}
 
-	// public CompletableFuture<JsonResponse> post(String path, List<MultipartFormdataPart> parts) throws DiscordAPIException {
-	// 	final var sb = new StringBuilder("");
-	// 	// TODO: Finish this: https://discord.com/developers/docs/reference#uploading-files
-	// }
+	// https://discord.com/developers/docs/reference#uploading-files
+	public CompletableFuture<JsonResponse> post(String path, List<MultipartFormdataPart> parts) throws DiscordAPIException, IOException {
+		final var baos = new ByteArrayOutputStream();
+		baos.write(("--" + MULTIPART_FORMDATA_BOUNDARY).getBytes());
+		for (final var p : parts) {
+			baos.write(p.toByteArray());
+			baos.write(("\n--" + MULTIPART_FORMDATA_BOUNDARY + '\n').getBytes());
+		}
+		baos.write("--".getBytes());
+		return sendRequest(
+			beginRequest(path)
+				.POST(BodyPublishers.ofByteArray(baos.toByteArray()))
+				.header("Content-Type", "multipart/form-data; boundary=\"" + MULTIPART_FORMDATA_BOUNDARY + '"')
+				.build(),
+			path, "<contains binary data>"
+		);
+	}
 
 	public CompletableFuture<JsonResponse> put(String path, String body) throws DiscordAPIException {
-		return buildAndSend(HttpMethod.PUT, path, body);
+		return sendRequest(
+			beginRequest(path)
+				.PUT(BodyPublishers.ofString(body))
+				.header("Content-Type", "application/json")
+				.build(),
+			path, body
+		);
 	}
 
 	public CompletableFuture<JsonResponse> patch(String path, String body) throws DiscordAPIException {
-		return buildAndSend(HttpMethod.PATCH, path, body);
+		return sendRequest(
+			beginRequest(path)
+				.method("PATCH", BodyPublishers.ofString(body))
+				.header("Content-Type", "application/json")
+				.build(),
+			path, body
+		);
 	}
 
 	public CompletableFuture<JsonResponse> delete(String path) throws DiscordAPIException {
-		return buildAndSend(HttpMethod.DELETE, path, null);
+		return sendRequest(beginRequest(path).DELETE().build(), path, null);
 	}
 }
